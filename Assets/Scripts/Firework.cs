ea04e0beb0ebbac1a13c116246d35a7d4a93016c
Blastermore <afonsocunha.dev@gmail.com>
2025-09-20 21:12:37 +0100

    Firework prefab

---
+++ Assets/Scripts/Firework.cs
@@ -0,0 +1,91 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.ShaderGraph;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class Firework : MonoBehaviour
+{
+    private int timeToExplode;
+    private Timer timer;
+    [SerializeField] private ParticleSystem [] fireworks;
+    [SerializeField] private ParticleSystem shockWave;
+    [SerializeField] private ParticleSystem trailSparks;
+    private Vector3 fireworkPos;
+    private TrailRenderer trail;
+    [SerializeField] private GameObject missileHeadObject;
+    [SerializeField] private ParticleSystem missileHead;
+
+    [SerializeField] private GameObject trailObject;
+    [ColorUsage(true, true)]
+    private Gradient chosenGradient;
+
+    [SerializeField] private List<Gradient> gradientList;
+    [SerializeField] private float shakeDuration = 0.5f;
+    [SerializeField] private float shakeMagnitude = 0.5f;
+
+    [MinMaxSlider(0f, 30f) ]
+    [SerializeField] private Vector2 timerRange;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [System.Obsolete]
+    void Start()
+    {
+        //Set up timer da juh
+        timer = new Timer(Random.Range(timerRange.x,timerRange.y), Timer.TimerReset.Manual);
+        timer.OnTimerDone += Burst;
+
+        //Get Particle System Component
+        fireworkPos = GetComponent<Transform>().position;
+
+        
+        var main1 = fireworks[0].main;
+        var main2 = fireworks[1].main;
+        var sparksMain = trailSparks.main;
+        var shockWaveMain = shockWave.main;
+        var missileMain = missileHead.main;
+
+
+        //Get Trail Renderer component
+        trail = GetComponentInChildren<TrailRenderer>();
+
+        //Based on the List of available color's length, get a random color
+        int i = Random.Range(0, gradientList.Count);
+        chosenGradient = gradientList[i];
+
+        //Set the particle system's stgradient to the selected color
+        main1.startColor = new ParticleSystem.MinMaxGradient(chosenGradient);
+
+        i = Random.Range(0, gradientList.Count);
+        chosenGradient = gradientList[i];
+        main2.startColor = new ParticleSystem.MinMaxGradient(chosenGradient);
+
+        sparksMain.startColor = new ParticleSystem.MinMaxGradient(chosenGradient);
+        shockWaveMain.startColor = new ParticleSystem.MinMaxGradient(chosenGradient);
+        missileMain.startColor = new ParticleSystem.MinMaxGradient(chosenGradient);
+
+
+        if (trail != null)
+        {
+            trail.colorGradient = chosenGradient;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer.CountTimer();
+    }
+
+    private void Burst()
+    {
+        trailObject.SetActive(false);
+        missileHeadObject.SetActive(false);
+        fireworks[0].Play();
+        shockWave.Play();
+        ScreenShakeManager.Instance.Shake(shakeDuration, shakeMagnitude);
+        Debug.Log("Rebentei");
+    }
+}
